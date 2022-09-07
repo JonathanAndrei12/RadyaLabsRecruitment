ab8eee482bf3ac83cf44b9d1700c19fc6bd7c54e
JonathanAndrei12 <jonathanandreiwibawa@gmail.com>
2022-09-07 17:33:13 +0700

    Initial Commit

---
+++ RadyaLabs Recruitment/RadyaLabs_RecruitmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadyaLabs_RecruitmentApp.swift
+//  RadyaLabs Recruitment
+//
+//  Created by Jonathan Andrei on 07/09/22.
+//
+
+import SwiftUI
+
+@main
+struct RadyaLabs_RecruitmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
